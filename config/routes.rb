b6f8d7c5961ff4a6d6acc86d5427e2f7fba6fd80
Kevin Rodriguez <kevin.1296@hotmail.com>
2017-08-27 16:53:36 -0600

    Cambios a nivel de configuraciond de base de datos

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+	resources :usuarios, only: [:create, :index, :show, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
